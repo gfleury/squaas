0e22693f7028220011ab9666a5f4f42fab322b14
gfleury <gfleury@gmail.com>
2019-08-27 11:05:36 +0200

    Fourth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,14 +11,24 @@ import DBqueryBench from 'd_bquery_bench';
 
 import './App.css';
 
-
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
+);
 
 function App() {
   var api = new DBqueryBench.QueryApi();
 
   api.apiClient.basePath = "http://localhost:8080/v1";
 
-  api.apiClient.defaultHeaders = { "Authorization": "Basic YWRtaW46YWRtaW4=" }
+  if (isLocalhost) {
+    api.apiClient.defaultHeaders = { "Authorization": "Basic YWRtaW46YWRtaW4=" }
+  }
 
   return (
     <div className="App">
