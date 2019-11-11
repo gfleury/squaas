880fdb996b3e46b3e8118aefdcac7293abaa282a
gfleury <gfleury@gmail.com>
2019-11-11 16:58:09 +0100

    Changing to use own simple logging subsystem

--- log/logger.go
+++ log/logger.go
@@ -7,16 +7,16 @@ import (
 )
 
 func Printf(format string, v ...interface{}) {
-	log.Printf(fmt.Sprintf("[GIN] %v | %s",
+	log.Printf(fmt.Sprintf("[INFO] %v | %s",
 		time.Now().Format("2006/01/02 - 15:04:05"), format), v)
 }
 
 func Fatalf(format string, v ...interface{}) {
-	log.Fatalf(fmt.Sprintf("[GIN] %v | %s",
+	log.Fatalf(fmt.Sprintf("[FATAL] %v | %s",
 		time.Now().Format("2006/01/02 - 15:04:05"), format), v)
 }
 
 func Fatal(v ...interface{}) {
-	log.Fatal(fmt.Sprintf("[GIN] %v | ",
+	log.Fatal(fmt.Sprintf("[FATAL] %v | ",
 		time.Now().Format("2006/01/02 - 15:04:05")), v)
 }
